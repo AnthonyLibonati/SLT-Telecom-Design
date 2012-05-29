651a45b0f4130ef772ac21842843891c4c59e1c8
sean burles <seanburles@gmail.com>
2012-05-28 22:55:36 -0700

    testimonial page jquery work

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,6 @@
+$(document).ready(function(){
+
+
 
  $(function() {
 // OPACITY OF BUTTON SET TO 0%
@@ -24,4 +27,25 @@ opacity: 0
 
 
 
+
+
+  
+
+
+
 	
+	
+
+
+
+$(".hide-wrapper #clickme").click(function () {
+     var parent_target = $(this).parents('.hide-wrapper');
+     if ($(parent_target).hasClass('active')) {
+          $(".house", parent_target).slideUp(200);
+          $(parent_target).removeClass('active');
+     } else {
+          $(".house", parent_target).slideDown(200);
+          $(parent_target).addClass('active');     }
+});  
+
+});// end document ready
