5a3ea42da35e28a8f2c7eb6846633cec46a6f02b
sean burles <seanburles@gmail.com>
2012-05-30 18:44:42 -0700

    naomi is a jerk

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -79,7 +79,7 @@ $(".hide-wrapper #clickme").click(function () {
 $('#slideshow').cycle({
 	fx: 'fade',
 	speed: 'slow',
-	timeout: 0,
+	timeout: 5000,
 	pager: '#slideNav',
 	slideExpr: '.hide-wrapper'
 
