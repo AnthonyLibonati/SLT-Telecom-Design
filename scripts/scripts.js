66d9aeea6b0e9b2011ac2d3f36e8537724bc82af
sean burles <seanburles@gmail.com>
2012-06-05 16:46:06 -0700

    home page jquery working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
+	$("img.contactBubble").css("opacity", 0);
+
+	$("img.contactBubble").hover(function(){
+		$(this).stop().fadeTo("slow", 1.0); 
+	},function(){
+   		$(this).stop().fadeTo("slow", 0.0); 
+	});
+});
 
 
 
 
-		
-	$(document).ready(function() {
-   $("a").click(function() {
-     alert("Hello world!");
-   });
- });
-
-
-
+$(document).ready(function() {
 	$('#clickme').click(function() {
     $('div',this).toggle();
 });
-
-	
-
+});
 
 
+
+$(document).ready(function() {
 $(".hide-wrapper #clickme").click(function () {
      var parent_target = $(this).parents('.hide-wrapper');
      if ($(parent_target).hasClass('active')) {
@@ -29,9 +29,10 @@ $(".hide-wrapper #clickme").click(function () {
           $(".house", parent_target).slideDown(200);
           $(parent_target).addClass('active');     }
 });  
+});
 
 //testimonial slide show
-
+$(document).ready(function() {
 $('#slideshow').cycle({
 	fx: 'fade',
 	speed: 'slow',
@@ -39,7 +40,8 @@ $('#slideshow').cycle({
 	pager: '#slideNav',
 	slideExpr: '.hide-wrapper'
 
-
+});  
 });
 
-});// end document ready
+
+
